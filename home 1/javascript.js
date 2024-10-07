c3501933ebd37010ec7741f21135e62f97a4ebb0
PandeleNitaAnotnio <pandele.antonio@gmail.com>
2024-10-08 01:36:52 +0300

    Home wark 2

--- home 1/javascript.js
+++ home 1/javascript.js
@@ -1,6 +1,7 @@
 document.getElementById("DiffButton").onclick = logic;
 Tf = document.getElementById("TorF");
 
+
 function logic ()
 {
     DividedNumber = document.getElementById("line").value;
@@ -16,5 +17,28 @@ function logic ()
 ///} else {console.Log (false)}
 
 ///console.log(DividedNumber)
+
+}
+document.getElementById("LaButton2").onclick =labutton2_0 ;
+Gorf = document.getElementById("Gorf");
+
+function labutton2_0 (){
+    D = document.getElementById("N").value;
+    C = document.getElementById("Z").value;
+    B = document.getElementById("Y").value;
+    A = document.getElementById("X").value;
+if (D !== ""&& C !== ""&& B !== ""&& A !== "") {
+
+    Gorf.innerHTML = `You will be a(N) ${A} in ${B}, married to ${C}, and you will have ${D} children.`
+
+} 
+else{
+
+Gorf.innerHTML = "Error" 
+
+
+}
+
+
 
 }
