a3e15ea9de2c104416a491c15fe92c2bfd4a2a9b
PandeleNitaAnotnio <pandele.antonio@gmail.com>
2024-10-08 01:04:50 +0300

    Revert "home work 1"
    This reverts commit f2349c1a6a029dbe45f7b43765a9fc7604e3edec.

--- home 1/javascript.js
+++ home 1/javascript.js
@@ -1,15 +1,20 @@
 document.getElementById("DiffButton").onclick = logic;
 Tf = document.getElementById("TorF");
+
 function logic ()
 {
     DividedNumber = document.getElementById("line").value;
-    x = DividedNumber % 10; 
-    console.log(x === 0)
+    if (DividedNumber === ""){
+        Tf.innerHTML = "Error" }
+    else{
+        x = DividedNumber % 10; 
+        Tf.innerHTML = (x === 0)
+        }
+    
+    
 //if (x === 0) { console.Log (True)
 ///} else {console.Log (false)}
 
-console.log(DividedNumber)
-
-Tf.innerHTML = (x === 0)
+///console.log(DividedNumber)
 
 }
